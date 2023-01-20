ff95191878c13ceec5d23f2c9514967cf90cfba6
Kári <kari@Karis-MacBook-Air-2.local>
2023-01-20 02:35:43 +0000

    Starting on some stuff

---
+++ Vika2/fkhi19.log.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+
+  for (int i = 0; i < t; i++) {
+    string bandname;
+    cin >> bandname;
+    int len = bandname.length();
+  }
+
+  return 0;
+}
