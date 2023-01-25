982025e039cb32ba4a0175ee40573cb3bb28978a
Kári <kari@Karis-MacBook-Air-2.local>
2023-01-25 11:48:23 +0000

    Hackerrssss

--- Vika2/fkhi19.reiknirit/fkhi19.reiknirit.cpp
+++ Vika2/fkhi19.reiknirit/fkhi19.reiknirit.cpp
@@ -18,35 +18,31 @@ int main() {
   ll n;
   cin >> n;
   
-  if (n == 0) cout << 0 << endl;
- 
-  else {
-    vector<ll> seq; // integer sequence
-    for (ll k = 0; k < n; k++) {
-      ll t;
-      cin >> t;
-      seq.push_back(t);
-    }
-
-    sort(seq.begin(), seq.end(), cmp);
-    vector<ii> nums;
-
-    for (ll i = 0, j = 0; j <= seq.size(); j++) {
-      if (seq[i] != seq[j]) {
-        nums.push_back(make_pair(seq[i], j - i));
-        i = j;
-      }
-    }
-
-    ll num_elements = n;
-    sort(nums.begin(), nums.end(), cmp_ii);
-    for (ll i = 0; i < nums.size(); i++) {
-      n -= nums[i].second;
-      num_elements += n;
-    }
-      
-    cout << num_elements << endl;
+  vector<ll> seq; // integer sequence
+  for (ll k = 0; k < n; k++) {
+    ll t;
+    cin >> t;
+    seq.push_back(t);
   }
+
+  sort(seq.begin(), seq.end(), cmp);
+  vector<ii> nums;
+
+  for (ll i = 0, j = 0; j <= seq.size(); j++) {
+    if (seq[i] != seq[j]) {
+      nums.push_back(make_pair(seq[i], j - i));
+      i = j;
+    }
+  }
+
+  ll num_elements = n;
+  sort(nums.begin(), nums.end(), cmp_ii);
+  for (ll i = 0; i < nums.size(); i++) {
+    n -= nums[i].second;
+    num_elements += n;
+  }
+    
+  cout << num_elements << endl;
 
   return 0;
 }
