f5fb7ff7d3522f2c7a32cbb4025a1cf6e8dbce28
LVTHNN <lvthnn@pop-os.localdomain>
2023-02-19 14:24:05 +0000

    Finished tildes

--- Vika6/tildes/tildes.cpp
+++ Vika6/tildes/tildes.cpp
@@ -17,7 +17,11 @@ class UnionFind {
     };
 
     int find(int p) {
-      while (p != id[p]) p = id[p];
+      while (p != id[p]) {
+        id[p] = id[id[p]];
+        p = id[p];
+      }
+
       return p;
     }
 
@@ -26,35 +30,33 @@ class UnionFind {
       int j = find(q);
       if (i == j) return;
 
-      if (sz[i] < sz[j]) { id[i] = j; sz[j] += sz[i]; cout << "Size of " << j << ": " << sz[j] << endl; }
-      else               { id[j] = i; sz[i] += sz[j]; cout << "Size of " << i << ": " << sz[i] << endl; }
+      if (sz[i] < sz[j]) { id[i] = j; sz[j] += sz[i]; }
+      else               { id[j] = i; sz[i] += sz[j]; }
     }
 };
-
-void read_query(UnionFind uf) {
-  char t;     // query type
-  int p1, p2; // parameters
-  
-  cin >> t;
-  if (t == 't') {
-    cin >> p1 >> p2;
-    p1 -= 1; p2 -= 1;
-    uf.union_of(p1, p2);
-  }
-
-  else if (t == 's') {
-    cin >> p1;
-    cout << uf.sz[uf.find(p1)] << endl;
-  }
-}
 
 int main() {
   int n, q;
   cin >> n >> q;
 
   UnionFind uf(n);
-  for (int i = 0; i < q; i++)
-    read_query(uf);
+  for (int i = 0; i < q; i++) {
+    char t;     // query type
+    int p1, p2; // parameters
+    
+    cin >> t;
+    if (t == 't') {
+      cin >> p1 >> p2;
+      p1 -= 1; p2 -= 1;
+      uf.union_of(p1, p2);
+    }
+
+    else if (t == 's') {
+      cin >> p1;
+      cout << uf.sz[uf.find(p1-1)] << endl;
+    }
+
+  }
 
   return 0;
 }
