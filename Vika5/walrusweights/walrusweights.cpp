e92ec05dc56f05a75802dca2507d68db1d481f26
LVTHNN <lvthnn@pop-os.localdomain>
2023-02-19 15:29:06 +0000

    Walrus!

--- Vika5/walrusweights/walrusweights.cpp
+++ Vika5/walrusweights/walrusweights.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
 #include <vector>
+#define MAXN 1000000
 
 using namespace std;
-
-int walrus(vector<int> a) {
-
-}
 
 int main() {
   int n;
   cin >> n;
 
-  vector<int> v;
+  int nums[n];
   for (int i = 0; i < n; i++) {
     int t;
     cin >> t;
-    
-    v.push_back(t);
+
+    nums[i] = t;
   }
 
-  cout << walrus(v, v.size() - 1, 1000) << endl;
+  bool w[MAXN] = {0};
+
+  for (int i = 0; i < n; i++) {
+    int wcurr = nums[i];
+    for (int j = sizeof(w) - 1; j >= 0; j--)
+      if (w[j] == true && j+wcurr < sizeof(w))
+        w[j + wcurr] = true;
+
+    w[wcurr] = true;
+  }
+
+  for (int i = 0; i < sizeof(w); i++)
+    if (w[i] == true) cout << i << endl;
 
 }
